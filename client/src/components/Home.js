a72622136b18ff5c346d24f0c0a419bb4747ccd8
heizlki <kirill.heitzler@gmail.com>
2020-12-20 12:14:00 +0100

    Mapped data on site

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,25 +1,28 @@
-import { useLazyQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import { stackQuery } from '../queries/queries';
 
 export default function Home() {
-	const [getData, { loading, error, data }] = useLazyQuery(stackQuery);
 
-	const runQuery = () => {
-		getData();
-		for (var i = 0; i < Object.keys(data.stacks).length; i += 1) {
-			console.log(data.stacks[i].title);
-			for (var x = 0; x < Object.keys(data.stacks[i].notes).length; x += 1) {
-				console.log(data.stacks[i].notes[x].title);
-			};
-		};
-	}
+	const { loading, error, data } = useQuery(stackQuery);
   if (loading) return 'Loading...';
   if (error) return `Error! ${error.message}`;
-
 	return (
 		<div className="App">
 			<p>Home</p>
-				<button onClick={runQuery}>getData</button>
+				{
+					data.stacks.map((stack) => {
+						return (
+							<div>
+								<li key={stack.stackID}>{stack.title}</li>
+								{stack.notes.map((note) => {
+									return (
+										<li key={note.noteID}>{note.title}</li>
+									);
+								})}
+							</div>
+						)
+					})
+				}
 		</div>
 	);	
 };
